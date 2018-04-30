95da244bdb77c829a9991870f57da8945c329cf8
Sil <sil@matise.nl>
2018-04-30 22:28:41 +0200

    add nuxt-link posibility

--- src/components/menu/MenuItem.vue
+++ src/components/menu/MenuItem.vue
@@ -1,21 +1,35 @@
 <template>
 	<li :class="module + '__item ' + classNames.item">
-		<a :class="module + '__link ' + classNames.link" :href="theLink">
+		<a v-if="smooth && !to" :class="module + '__link ' + classNames.link" :href="theLink" v-smooth-scroll>
 			<span :class="module + '__text ' + classNames.text">
 				<slot></slot>
 			</span>
 		</a>
+		<a v-if="!smooth && !to" :class="module + '__link ' + classNames.link" :href="theLink">
+			<span :class="module + '__text ' + classNames.text">
+				<slot></slot>
+			</span>
+		</a>
+		<nuxt-link v-if="to" :class="module + '__link ' + classNames.link" :to="theLink">
+			<span :class="module + '__text ' + classNames.text">
+				<slot></slot>
+			</span>
+		</nuxt-link>
 	</li>
 </template>
 
 <script>
+import vueSmoothScroll from 'vue-smooth-scroll'
+Vue.use(vueSmoothScroll);
+
 	export default{
 	  name: 'menu-item',
-		props: ['mod','text', 'link'],
+		props: ['mod','text', 'link', 'to','smooth'],
 		data() {
 			return{
 				module: 'nav',
 				theLink: '#',
+				smoothScroll: false,
 				classNames: {
 					item: [],
 					link: [],
@@ -29,6 +43,9 @@
 			}
 			if(this.$props.link){
 				this.theLink = this.$props.link;
+			}
+			if(this.$props.smooth){
+				this.smoothScroll = true;
 			}
 		}
 	}
