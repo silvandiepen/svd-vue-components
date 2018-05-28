5a32db6a1ec0c91332a3ffb31633ed670c3016aa
Sil <sil@matise.nl>
2018-05-28 08:23:33 +0200

    add four-fifth column size

--- src/components/grid/methods.js
+++ src/components/grid/methods.js
@@ -1,17 +1,18 @@
 let classNames;
 
 const settings = {
-    breakpoints: ["small", "medium", "large", "xlarge", "xxlarge"],
-    partials: {
-        full: 1,
-        half: 0.5,
-        third: 0.33,
-        quarter: 0.25,
-        fifth: 0.2,
-        sixth: 0.16,
-        "two-third": 0.67,
-        "three-quarter": 0.75
-    }
+  breakpoints: ["small", "medium", "large", "xlarge", "xxlarge"],
+  partials: {
+    full: 1,
+    half: 0.5,
+    third: 0.33,
+    quarter: 0.25,
+    fifth: 0.2,
+    sixth: 0.16,
+    "two-third": 0.67,
+    "three-quarter": 0.75,
+    "four-fifth": 0.8
+  }
 };
 
 export default {
